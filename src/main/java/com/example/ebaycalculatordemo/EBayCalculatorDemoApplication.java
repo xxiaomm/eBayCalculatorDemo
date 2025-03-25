f39dd2d6e145fc7026cb5f60c524d5d6d624857b
xxiaomm <xm2074@nyu.edu>
2025-03-25 15:59:59 -0400

    first commit

---
+++ src/main/java/com/example/ebaycalculatordemo/EBayCalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebaycalculatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBayCalculatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBayCalculatorDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ebaycalculatordemo/EBayCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebaycalculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBayCalculatorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
